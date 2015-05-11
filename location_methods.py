3f01804aa75f9c9d4a361a050de8ba8ed3a0aba7
Aashish Chaudhary <aashish.chaudhary@kitware.com>
2015-05-10 23:15:35 -0400

    Added multiprocessing for filtering

--- location_methods.py
+++ location_methods.py
@@ -7,18 +7,14 @@ Created on Thu Apr 02 15:10:42 2015
 import json
 from LocationResolver import LocationResolver
 
-# Returns True if successful, false otherwise
-def location_filter(location, db, s_coll, t_coll):
-    ## list of tweets
-    resolver = LocationResolver.getLocationResolver()
-    for tweet in s_coll.find():
+def execute(range):
+    for tweet in s_coll.find(range[0]:range[1]):
         try:
-        ##   IDEA:  GUI to choose different locations
-        ##          -> Just put locations into list
-        ##          -> GUI button chooses which location (list index)
-        ##          --> i.e. locations = [in_USA, in_CANADA, in_XYZ]
-        ##          ---> maybe get complete listing of locations from carmen?
-            #tweet = json.loads(tweet)
+            ##   IDEA:  GUI to choose different locations
+            ##          -> Just put locations into list
+            ##          -> GUI button chooses which location (list index)
+            ##          --> i.e. locations = [in_USA, in_CANADA, in_XYZ]
+            ##          ---> maybe get complete listing of locations from carmen?
             if 'mental_illness' not in tweet:
                 tweet['mental_illness'] = 0
             if 'physical_illness' not in tweet:
@@ -28,18 +24,29 @@ def location_filter(location, db, s_coll, t_coll):
                 if not list(t_coll.find({ 'id': tweet['id'] })):
                     if not t_coll.insert(tweet):
                         return False
-
         except:
-            ##print "No Place key"
             pass
-    #successful if made it here        
+
+# Returns True if successful, false otherwise
+def location_filter(location, db, s_coll, t_coll):
+    from multiprocessing import Pool
+    ## list of tweets
+    resolver = LocationResolver.getLocationResolver()
+    count = s_coll.find().count()
+    p = Pool(10)
+    start_index = 0
+    end_index = 0
+    for i in xrange(0, 10, 1):
+        end_index += count / 10.0
+        p.map(execute, [start_index, end_index])
+        start_index = end_index
     return True
 
 ## Determines whether the tweet_loc tweet location matches any contained
 ##      in the q_loqs location list.
 def getBool(tweet_loc, q_locs):
     ## Trivially matches
-    if not q_locs: 
+    if not q_locs:
         return true
 
     for item in q_locs:
